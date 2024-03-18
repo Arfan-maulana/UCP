f8ff98409b627e17ed8d7095b3d6733d63b84647
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2024-03-18 09:56:30 +0700

    env, mvc, route, relationship

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('todo.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('todo.create');
+    }
+
+
+    public function edit(Todo $todo)
+    {
+        return view('todo.edit');
+    }
+
+}
