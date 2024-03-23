df46a648d4851d5e3d80d684195a6520fa70efcd
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2024-03-23 15:53:42 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +17,19 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+          User::factory()->create([
+             'name' => 'Admin',
+             'email' => 'admin@admin.com',
+             'is_admin'=> true
+        ]);
+
+        User::factory()->create([
+            'name' => 'Muhammad Arfan Maulana',
+            'email' => 'muhammad.arfan.ft21@mail.umy.ac.id',
+
+       ]);
+
+        User::factory(100)-> create();
+        Todo::factory(500)-> create();
     }
 }
